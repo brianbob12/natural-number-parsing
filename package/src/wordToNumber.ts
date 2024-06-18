78c21bf2515fe3216712e60980296392908c3515
Cyrus Singer <japaneserhino@gmail.com>
2024-06-18 15:16:58 -0400

    finish setting up 1.0

--- package/src/wordToNumber.ts
+++ package/src/wordToNumber.ts
@@ -1,24 +1,77 @@
+
+const ONES = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+const TEENS = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+const TENS = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+const SCALES = ["", "thousand", "million", "billion", "trillion"]
+
+
+
+const isHyphenatedNumber = (word:string):null|[string,string] => {
+  const splits = word.split('-')
+  if(splits.length !== 2){
+    return null
+  }
+  if( ONES.indexOf(splits[1]) <= 1 ){ //word-zero is not a split
+    return null
+  }
+  if( TENS.indexOf(splits[0]) <= 1 ){ //word-zero is not a split
+    return null
+  }
+  return [splits[0], splits[1]]
+}
+
+const breakUpHyphenatedNumbers = (words:string[]):string[] => {
+  const newWords:string[] = []
+  for(let i = 0; i < words.length; i++){
+    const split = isHyphenatedNumber(words[i])
+    if(split !== null){
+      newWords.push(split[0])
+      newWords.push(split[1])
+    }
+    else{
+      newWords.push(words[i])
+    }
+  }
+  return newWords
+}
+
 /**
  * 
  * @param input a sting of words to attempt to convert to a number
  * @returns [the number, or null if the input is not a number, if the input can be extended to a number]
  */
 export const wordToNumber = (input:string):[number|null,boolean] => {
-  const ones = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-  const teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
-  const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-  const scales = ["", "thousand", "million", "billion", "trillion"]
-
   let number = 0
   let currentNumber = 0
   let isDecimal = false
   let decimalMultiplier = 0.1
+  let isNegative = false
 
   let moreNumbersNeeded = false
 
   const INVALID_NUMBER:[number|null,boolean] = [null,false]
 
-  const words = input.replace(/,/g,"").split(" ");
+  //  #1  words pre-processing
+  let words = input.replace(/,/g,"").split(" ");
+
+  //handle leading minus
+  if(words.length > 0 && 
+    (words[0] === "minus" || words[0] === "negative")){
+    words.shift()
+    moreNumbersNeeded = true
+    isNegative = true
+  }
+  else if(words.length > 0 && words[0].startsWith("-")){
+    isNegative = true
+    words[0] = words[0].substring(1)
+    moreNumbersNeeded = true
+  }
+  
+  //break up hyphenated numbers
+  words = breakUpHyphenatedNumbers(words)
+
+  //  #2  number building
+
   for(let i = 0; i < words.length; i++){
     if(words[i] === ""){
       continue;
@@ -38,6 +91,9 @@ export const wordToNumber = (input:string):[number|null,boolean] => {
     moreNumbersNeeded = false
 
     if(words[i].match(/^[0-9\\.]+$/)){
+      if(i != 0){
+        return INVALID_NUMBER
+      }
       if(currentNumber !== 0){
         return INVALID_NUMBER
       }
@@ -45,6 +101,7 @@ export const wordToNumber = (input:string):[number|null,boolean] => {
       if(isNaN(digit)){
         return INVALID_NUMBER
       }
+      
       currentNumber = digit
       continue
     }
@@ -53,13 +110,18 @@ export const wordToNumber = (input:string):[number|null,boolean] => {
       if(currentNumber === 0){
         currentNumber = 1
       }
-      if(isDecimal || currentNumber%100 === 0){
+      if(
+        isDecimal ||
+        currentNumber%100 === 0 ||
+        currentNumber%100 === 10 ||
+        currentNumber%100>19
+      ){
         return INVALID_NUMBER
       }
       currentNumber *= 100
       continue
     }
-    let scaleIndex = scales.indexOf(word);
+    let scaleIndex = SCALES.indexOf(word);
     if(scaleIndex !== -1){
       if(currentNumber === 0){
         currentNumber = 1
@@ -71,18 +133,27 @@ export const wordToNumber = (input:string):[number|null,boolean] => {
           return INVALID_NUMBER
         }
         number = currentNumber * Math.pow(1000, scaleIndex)
+        if(isNegative){
+          number = -number
+        }
         return [number, true]
       }
-      if(isDecimal || currentNumber%1000 === 0){
+      const multiplier = Math.pow(1000, scaleIndex)
+      if(
+        isDecimal ||
+        currentNumber%1000 === 0 ||
+        currentNumber>=multiplier ||
+        (number/multiplier)%1000 !== 0 //could conflict
+      ){
         return INVALID_NUMBER
       }
-      currentNumber *= Math.pow(1000, scaleIndex);
+      currentNumber *= multiplier
       number += currentNumber
       currentNumber = 0
       continue
     }
 
-    let onesIndex = ones.indexOf(word);
+    let onesIndex = ONES.indexOf(word);
     if(onesIndex !== -1){
       if((!isDecimal) && currentNumber%10 !== 0){
         return INVALID_NUMBER
@@ -96,7 +167,7 @@ export const wordToNumber = (input:string):[number|null,boolean] => {
       }
       continue
     }
-    let teensIndex = teens.indexOf(word);
+    let teensIndex = TEENS.indexOf(word);
     if(teensIndex !== -1){
       if(isDecimal || currentNumber%100 !== 0){
         return INVALID_NUMBER
@@ -104,7 +175,7 @@ export const wordToNumber = (input:string):[number|null,boolean] => {
       currentNumber += teensIndex + 10
       continue
     }
-    let tensIndex = tens.indexOf(word);
+    let tensIndex = TENS.indexOf(word);
     if(tensIndex !== -1){
       if(isDecimal || currentNumber%100 !== 0){
         return INVALID_NUMBER
@@ -127,6 +198,8 @@ export const wordToNumber = (input:string):[number|null,boolean] => {
     //clip to decimal multiplier to avoid floating point errors
     number = +number.toFixed(-Math.log10(decimalMultiplier))
   }
-
+  if(isNegative){
+    number = -number
+  }
   return [number, true]
 }
